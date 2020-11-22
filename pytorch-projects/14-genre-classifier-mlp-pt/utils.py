5cf44aae8d780e1dde0a4c5024b32169c8366e55
lucaseo <lucaseo1991@gmail.com>
2020-11-22 23:18:49 +0900

    removed rounding up.

--- pytorch-projects/14-genre-classifier-mlp-pt/utils.py
+++ pytorch-projects/14-genre-classifier-mlp-pt/utils.py
@@ -1,11 +1,10 @@
 import torch
 
 
-def multi_acc(y_pred, y_test):
+def multi_acc(y_pred, y_true):
     _, y_pred_tags = torch.max(y_pred, dim=1)
 
-    correct_pred = (y_pred_tags == y_test).float()
+    correct_pred = (y_pred_tags == y_true).float()
     acc = correct_pred.sum() / len(correct_pred)
-    acc = torch.round(acc)
 
     return acc
